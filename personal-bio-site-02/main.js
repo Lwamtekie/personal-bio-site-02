4e029c1933a6a53b80f19e2f30862bf58a7ec88f
Lwam Tekie <ltekie87@yahoo.com>
2019-06-03 18:18:14 -0500

    wip

--- personal-bio-site-02/main.js
+++ personal-bio-site-02/main.js
@@ -1,1 +1,11 @@
-console.log 
+const app = {
+  pages: [],
+  show: new Event('show'),
+  init: function(){
+    app.pages = documnet.querySelectorAll('.page');
+    app.pages.forEach((pg)=>{
+        pg.addEvenlistner('show', app.pageshown);
+      })
+      document.querySelectorAL
+  }
+}
