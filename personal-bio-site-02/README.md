06e18652dc63ce7abe2d6660e44a878aa9891780
Lwam Tekie <ltekie87@yahoo.com>
2019-07-09 17:36:53 -0500

    wip

--- personal-bio-site-02/README.md
+++ personal-bio-site-02/README.md
@@ -12,9 +12,12 @@ Personal site of one page, with basic html and javascript. And some styling for 
 
 #screenshot:
 
+![ screenshot](https://raw.githubusercontent.com/Lwamtekie/personal-bio-site-02/master/personal-bio-site-02/images/screencapture-127-0-0-1-8080-2019-07-09-14_16_04.png)
 
 
-#How To Run Clone this repository: git clone https://github.com/Lwamtekie/Personal-bio-site- Go into the repository cd personal-site Run http server hs -p 5000
+#How To Run:
+
+ Clone this repository: git clone https://github.com/Lwamtekie/Personal-bio-site- Go into the repository cd personal-site Run http server hs -p 5000
 
 #Author:
 
--- personal-bio-site-02/main.js
+++ personal-bio-site-02/main.js
@@ -6,7 +6,7 @@ const printToDom = (divId, textToPrint) => {
     {
       title: "Product Cards",
       screenshot:
-        "https://github.com/Lwamtekie/product-cards/blob/master/screenshot/screencapture-127-0-0-1-8082-2019-05-18-12_05_01.png",
+        "https://raw.githubusercontent.com/Lwamtekie/product-cards/master/screenshot/screencapture-127-0-0-1-8082-2019-05-18-12_05_01.png",
       description: "Product cards using html, css, and flexbox for positioning",
       technologiesUsed: "HTML and CSS",
       available: true,
@@ -15,7 +15,7 @@ const printToDom = (divId, textToPrint) => {
     {
       title: "Sorting-hat",
       screenshot:
-        "https://github.com/Lwamtekie/Sorting-hat/blob/master/screenshot/Screen%20Shot%202019-05-02%20at%206.20.56%20PM.png",
+        "https://raw.githubusercontent.com/Lwamtekie/Sorting-hat/master/screenshot/Screen%20Shot%202019-05-02%20at%206.20.56%20PM.png",
       description:
         " In this project we used the buttons in index.html to work by using the eventlisner function in java script. And by building the container in the domString builder in the javascript. ",
       technologiesUsed: " index.html, main.js, main.css",
@@ -25,7 +25,7 @@ const printToDom = (divId, textToPrint) => {
     {
       title: "Temperature-Converter",
       screenshot:
-        "https://github.com/Lwamtekie/Temperature-Converter/blob/master/screenshot/screencapture-127-0-0-1-8081-2019-05-21-11_43_18.png",
+        "https://raw.githubusercontent.com/Lwamtekie/Temperature-Converter/master/screenshot/screencapture-127-0-0-1-8081-2019-05-21-11_43_18.png",
       description:
         "The application show the conversion of temperature from fahrenheit to celsius, and celsius to fahrenheit.",
       technologiesUsed: "index.html, main.js, main.css",
@@ -35,7 +35,7 @@ const printToDom = (divId, textToPrint) => {
     {
       title: "favorite Places",
       screenshot:
-        "https://github.com/Lwamtekie/favorite_places/blob/master/screenshots/screencapture-127-0-0-1-8083-2019-05-18-12_42_26.png",
+        "https://raw.githubusercontent.com/Lwamtekie/favorite_places/master/screenshots/screencapture-127-0-0-1-8083-2019-05-18-12_42_26.png",
       description:
         "This projectis different form the pervious assignments, because all the data for favorite places is in Json file not in main.js. So main.js has only the Print To Dom and the domString to style the bootstrap card. The styling and color is in main.css.",
       technologiesUsed: "index.html, main.css, main.js, json.data(db)",
@@ -45,7 +45,7 @@ const printToDom = (divId, textToPrint) => {
     {
       title: "sandwich-maker",
       screenshot:
-        "https://github.com/Lwamtekie/sandwich-maker/blob/master/screenshot/screencapture-127-0-0-1-8080-2019-05-16-23_38_05.png",
+        "https://raw.githubusercontent.com/Lwamtekie/sandwich-maker/master/screenshot/screencapture-127-0-0-1-8080-2019-05-16-23_38_05.png",
       description:
         "The Foodie Club is similar with the online checkout style cart. it helps to secure the final price by passing information through modules.",
       technologiesUsed:
@@ -112,12 +112,15 @@ const printToDom = (divId, textToPrint) => {
   const createProjectCard = (array) => {
     let domString = "";
     array.forEach(project => {
-      domString += `<div class="projectDiv">`;
-      domString += `<p class="title">${project.title}</p>`;
-      // domString += `<p><img class= "screenshot" src=${project.screenshot}></p>`;
+      domString += '<div class="col-4 product">';
+      domString +=   `<div class="card">`;
+      domString += `<h2 class="title">${project.title}</h2>`;
+      domString += `<img class= "screenshot" src=${project.screenshot}></<img>`;
+      domString +=   `<div class="card-body">`;
       domString += `<p class="description">${project.description}</p>`;
-      domString += `<p class="tech">${project.technologiesUsed}</p>`;
+      domString += `<h3 class="tech">${project.technologiesUsed}</h3>`;
       domString += `<p class="link">Check it out <a href={project.url}>HERE</a></p>`;
+      domString += `</div>`;
       domString += `</div>`;
     })
     printToDom('ProjectPage', domString);
